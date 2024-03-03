100ed4a7232682297a8b2db289037939a939a9c7
Erin1101 <87416956+Erin1101@users.noreply.github.com>
2024-03-03 16:58:06 +0800

    batch size

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3215dec0340cfed3c660d7ce3411d682454b0a9a1577217bf688d19bcbbec191
-size 5853
+oid sha256:cc57fe23521b83a9ac476fc049ae0c7b64be29f0de390b82e2f9e0e3c99c6e6d
+size 5855
