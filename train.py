3d06a36657f799cfacceb2ca55f701d7517d3528
Erin1101 <87416956+Erin1101@users.noreply.github.com>
2024-03-03 17:29:10 +0800

    reduce

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc57fe23521b83a9ac476fc049ae0c7b64be29f0de390b82e2f9e0e3c99c6e6d
-size 5855
+oid sha256:762a955f6378412e07525bb2f645193663630cb9bc04f46c0faf0e24a43dfd79
+size 5854
